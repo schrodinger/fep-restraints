948024eb452b2468207fac5af4130fc8e61d5e65
Martin Voegele <martin.voegele@schrodinger.com>
2022-07-05 17:50:46 -0400

    Write components in cluster reference.

--- extract_clusters_as_trj.py
+++ extract_clusters_as_trj.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', dest='cms_file', type=str, help='cms file')
-    parser.add_argument('-t', nargs='+', dest='trj_files', type=str, help='trajecotry files or directories')    
+    parser.add_argument('-t', nargs='+', dest='trj_files', type=str, help='trajectory files or directories')    
     parser.add_argument('-n', nargs='+', dest='frame_number_files', type=str, help='files with frame numbers (zero-based) and properties')
     parser.add_argument('-p', dest='property', type=str, help='name of the column where the cluster ID is saved', default='Cluster_ID')
     parser.add_argument('-d', dest='definitions_file', type=str, help='csv file with definitions of cluster IDs, input files, and centroids. Input trajectories must be passed in the order that corresponds to the feature files', default=None)
@@ -28,8 +28,11 @@ if __name__ == "__main__":
     # Write all CMS files
     _, cms_model = topo.read_cms(args.cms_file)
     #aid_list = cms_model.select_atom(args.selection)
+    model = cms_model.copy()
     with structure.StructureWriter(args.output_name+'.cms') as writer:
-        writer.append(cms_model.fsys_ct)
+        writer.append(model.fsys_ct)
+        for st in model.comp_ct:
+            writer.append(st)
 
     # Find all cluster IDs
     if args.definitions_file is None:
