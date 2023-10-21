6db8233dece560ef9905d9bce10e2137b5ddbe8a
Martin Voegele <martin.voegele@schrodinger.com>
2023-10-20 22:57:32 -0400

    add standalone scripts for backbone and sidechain torsions

---
+++ tasks/read_bb_torsions_from_trajectory.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from io_trajectory import load_trajectory
+from io_features import write_features_to_csv, calculate_backbone_torsions
+
+
+if __name__ == "__main__":
+    """ 
+    Calculates the backbone torsions (Ramachandran angles) for all selected residues. 
+    """
+       
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', dest='cms_file', type=str, help='cms file')
+    parser.add_argument('-x', dest='xtc_file', type=str, help='xtc file', default=None)
+    parser.add_argument('-t', dest='trj_dir', type=str, help='trajecotry directory', default=None)    
+    parser.add_argument('-n', nargs='+', dest='residue_names', type=str, help='residue names', default=None)
+    parser.add_argument('-r', nargs='+', dest='residue_numbers', type=int, help='residue numbers', default=None)
+    parser.add_argument('-l', dest='chain_id', type=str, help='chain identifier', default='A')
+    parser.add_argument('-o', dest='outputfile', type=str, help='name of the output csv file', default='results.csv')            
+    args = parser.parse_args()
+
+    # load data
+    msys_model, cms_model, trajectory = load_trajectory(
+        args.cms_file, 
+        args.xtc_file, 
+        args.trj_dir
+        )
+    # calculate the distances between the C-alpha atoms
+    time, dist_names, distances = calculate_backbone_torsions(
+        msys_model, cms_model, trajectory, 
+        args.chain_id, args.residue_numbers, 
+        residue_names=args.residue_names
+        )
+    # write the distances to a CSV file
+    write_features_to_csv(
+        args.outputfile, 
+        distances, 
+        dist_names, 
+        time
+        )
