d68fb492ac4f6a6f99969a000d3ff5f2b7bfb979
Martin Voegele <martin.voegele@schrodinger.com>
2025-07-16 14:38:26 -0400

    add option to add account to unrestrained ABFEP and add help strings to all flags.

--- setup_unrestrained_abfep.py
+++ setup_unrestrained_abfep.py
@@ -11,8 +11,8 @@ from schrodinger.forcefield.custom_params import create_archive_from_oplsdir, me
 
 
 def write_abfep_job_script(
-    job_name, fep_sim_time, md_sim_time, host, subhost, project, maxjob, retries, 
-    salt=None, ff='OPLS4', opls=None, ffhost=None, seed=2007
+    job_name, fep_sim_time, md_sim_time, host, subhost, maxjob, retries, 
+    salt=None, ff='OPLS4', opls=None, ffhost=None, seed=2007, project=None, account=None
     ):
 
     # Get the directory where the code is
@@ -43,21 +43,22 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Extract individual pv files from one FEP pv file.")
     parser.add_argument('mae_file', help="File with input structures for FEP")
     parser.add_argument('-j','--job-name', help="Name for the output directory")
-    parser.add_argument('--host', type=str, default='driver')
-    parser.add_argument('--subhost', type=str, default='gpu')
-    parser.add_argument('--project', type=str, default=None)
-    parser.add_argument('--maxjob', type=int, default=0)
-    parser.add_argument('--retries', type=int, default=5)
+    parser.add_argument('--host', type=str, default='driver', help='Host for the job submission. Default: driver')
+    parser.add_argument('--subhost', type=str, default='gpu', help='Subhost for the job submission. Default: gpu')
+    parser.add_argument('--project', type=str, default=None, help='Project name for the job submission (only on Bolt).')
+    parser.add_argument('--account', type=str, default=None, help='Account name for the job submission (only on Ada).')
+    parser.add_argument('--maxjob', type=int, default=0, help='Maximum number of jobs to run in parallel. Default: 0 (no limit).')
+    parser.add_argument('--retries', type=int, default=5, help='Number of retries for failed jobs. Default: 5.')
     parser.add_argument('--ff', choices=['OPLS4', 'OPLS5'], default='OPLS4', help='Specify the forcefield to use. Default: OPLS4.')
-    parser.add_argument('--oplsdir', type=str, default=None)
-    parser.add_argument('--ffbuilder', action='store_true')
-    parser.add_argument('--ffhost', type=str, default='cpu')
-    parser.add_argument('--md-sim-time', type=float, default=2000)
-    parser.add_argument('--fep-sim-time', type=float, default=10000)
-    parser.add_argument('--salt', type=float, default=None)
-    parser.add_argument('--overwrite', action='store_true')
-    parser.add_argument('--lig-rest', action='store_true')
-    parser.add_argument('--seed', type=int, default=2007)
+    parser.add_argument('--oplsdir', type=str, default=None, help='Path to the OPLS force field directory. Default: None.')
+    parser.add_argument('--ffbuilder', action='store_true', help='Start a force field builder job for missing parameters.')
+    parser.add_argument('--ffhost', type=str, default='cpu', help='Host for the force field builder job. Default: cpu.')
+    parser.add_argument('--md-sim-time', type=float, default=2000, help='MD simulation time in ps. Default: 2000.')
+    parser.add_argument('--fep-sim-time', type=float, default=10000, help='FEP simulation time in ps. Default: 10000.')
+    parser.add_argument('--salt', type=float, default=None, help='Salt concentration in Molar. Default: None (no salt).')
+    parser.add_argument('--overwrite', action='store_true', help='Overwrite the job directory if it already exists.')
+    parser.add_argument('--lig-rest', action='store_true', help='Use the ligand restraints from the input file. Default: False.')
+    parser.add_argument('--seed', type=int, default=2007, help='Random seed for the job. Default: 2007.')
     args = parser.parse_args()
 
     # Remove FFB host if no ff is generated
@@ -96,12 +97,14 @@ if __name__ == '__main__':
         args.job_name, 
         args.fep_sim_time, args.md_sim_time, 
         args.host, args.subhost, 
-        args.project, args.maxjob, args.retries, 
+        args.maxjob, args.retries, 
         salt=args.salt,
         ff=args.ff,
         opls=opls, 
         ffhost=args.ffhost,
-        seed=args.seed
+        seed=args.seed,
+        project=args.project,
+        account=args.account
     )        
     # Leave the job directory
     os.chdir('..')
